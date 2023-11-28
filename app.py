58fe32b70a2f5879e5576470d2cbe386e02b2ab8
jenssondesign <94224733+jenssondesign@users.noreply.github.com>
2023-11-28 14:13:50 +0000

    Lisätty avointen työpaikkojen kortit ja leiska parannettu

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -20,16 +20,12 @@ JOBS = [
     'title': 'Myyjä',
     'sijainti': 'Helsinki',
     'tuntipalkka': '14€'
-  },
+  }
 ]
 
 @app.route("/")
 def hello():
     return render_template('home.html', jobs=JOBS)
-
-@app.route("/api/tyopaikat")
-def list_jobs():
-  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
