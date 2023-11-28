8b6b622985bc4046a38dd8b8f4c183813ecda232
jenssondesign <94224733+jenssondesign@users.noreply.github.com>
2023-11-28 10:21:18 +0000

    lisätty dynaamista dataa ja API-polku

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Puutarhuri',
+    'sijainti': 'Helsinki',
+    'tuntipalkka': '16€'
+  },
+  {
+    'id': 2,
+    'title': 'Vuorovastaava',
+    'sijainti': 'Helsinki',
+    'tuntipalkka': '24€'
+  },
+  {
+    'id': 3,
+    'title': 'Myyjä',
+    'sijainti': 'Helsinki',
+    'tuntipalkka': '14€'
+  },
+]
+
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/tyopaikat")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
