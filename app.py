0f8b017dc6c8cab22a8ba6ecbf226a630c2bc4ab
jensondesign <94224733+jensondesign@users.noreply.github.com>
2023-11-30 17:26:53 +0000

    Lisätty dynaamiset sivut työpaikkoja varten

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify
 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 from sqlalchemy import text
 
@@ -15,6 +15,15 @@ def hello():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  
+  if not job:
+    return "Sivua ei löydy", 404
+    
+  return render_template('jobpage.html', job=job)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -20,3 +20,18 @@ def load_jobs_from_db():
   for row in result.all():
       jobs.append(dict(zip(result.keys(), row)))
   return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+      query = text("SELECT * FROM jobs WHERE id = :val")
+      print(f"Executing SQL query: {query}")
+      result = conn.execute(query, {'val': id})
+      row = result.fetchone()
+
+      if row is None:
+          return None
+      else:
+          # Convert RMKeyView to a list and then use indexing
+          column_names = list(result.keys())
+          row_dict = {column_names[i]: row[i] for i in range(len(row))}
+          return row_dict
